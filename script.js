1c7dcb4013bfe1eeeda077ebc555eb268ad87d91
Genta Wibowo <tetrahedral@gmail.com>
2021-03-22 23:12:32 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+import http from 'k6/http';
+import { check } from 'k6';
+
+export let options = {
+    vus: 2,
+    duration: '5s',
+  };
+
+
+export default function () {
+  var url = 'http://test.k6.io/login';
+  var payload = JSON.stringify({
+    email: 'aaa',
+    password: 'bbb',
+  });
+
+  var params = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+
+//   let res = http.post(url, payload, params);
+
+  let res = http.get('http://test.k6.io/')
+
+  check(res, {
+    'is status 200': (r) => r.status === 200,
+  });
+}
